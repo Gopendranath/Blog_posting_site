8504eac375e21f65c3cc4500724c249a3dabfac5
Gopendranath <baban567899@gmail.com>
2024-07-21 03:08:42 +0530

    new changes

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send(`Hello world`);
-})
+    const locals = {
+        title: "Nodejs Blog site",
+        description: "Simple Blog posting site created with nodejs express, ejs"
+    }
+    res.render(`index`,{ locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
 
 module.exports = router;
